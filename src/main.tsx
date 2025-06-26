02375fb28c7f055ed29c377bef7e20b5213895bc
Enrique G Zbinden <enriquezbinden04@gmail.com>
2025-06-26 02:28:12 +0200

    feat: add PWA service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { EventProvider } from './context/EventContext';
+import { registerSW } from 'virtual:pwa-register';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </EventProvider>
   </React.StrictMode>
 );
+
+registerSW({ immediate: true });
